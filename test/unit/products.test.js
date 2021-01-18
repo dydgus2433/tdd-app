1e490d480b5207e40969e374f4e992e4695c961c
yonghyun <dydgus2433@naver.com>
2021-01-19 06:05:53 +0900

    add product create test

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,3 +1,40 @@
-test("two plus two is four", () => {
-  expect(2 + 2).toBe(4);
+const productController = require("../../controller/products");
+const productModel = require("../../models/Product");
+const httpMocks = require("node-mocks-http");
+const newProduct = require("../data/new-product.json");
+productModel.create = jest.fn();
+let req;
+let res;
+let next;
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
+describe("Product Controller Create", () => {
+  beforeEach(() => {
+    req.body = newProduct;
+  });
+  it("should have a createProduct function", () => {
+    expect(typeof productController.createProduct).toBe("function");
+  });
+
+  it("should call ProductModel.create", () => {
+    productController.createProduct(req, res, next);
+    expect(productModel.create).toBeCalledWith(newProduct);
+  });
+
+  it("should return 201 response code", () => {
+    productController.createProduct(req, res, next);
+    expect(res.statusCode).toBe(201);
+
+    //send가 있다면
+    expect(res._isEndCalled()).toBeTruthy();
+  });
+
+  it("should return json body in response", () => {
+    productModel.create.mockReturnValue(newProduct);
+    productController.createProduct(req, res, next);
+    expect(res._getJSONData()).toStrictEqual(newProduct);
+  });
 });
